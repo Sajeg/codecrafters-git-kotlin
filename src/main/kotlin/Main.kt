fad07df604f68d7a8ed401bfd9360ae707541f68
Sajeg <84874195+Sajeg@users.noreply.github.com>
2025-09-26 22:15:16 +0000

    Completed second stage

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.util.zip.Inflater
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -9,19 +11,48 @@ fun main(args: Array<String>) {
         println("Usage: your_program.sh <command> [<args>]")
         exitProcess(1)
     }
+    when (args[0]) {
+        "init" -> {
+            val gitDir = File(".git")
+            gitDir.mkdir()
+            File(gitDir, "objects").mkdir()
+            File(gitDir, "refs").mkdir()
+            File(gitDir, "HEAD").writeText("ref: refs/heads/master\n")
 
-    if (args[0] == "init") {
-        // Uncomment this block to pass the first stage
+            println("Initialized git directory")
+        }
+        "cat-file" -> {
+            if (args[1] == "-p") {
+                val inputStream = File(".git/objects/${args[2].subSequence(0, 2)}/${args[2].subSequence(2, 40)}").inputStream()
+                val blob = inputStream.readAllBytes().zlibDecompress()
+                println(blob.split('\u0000')[1])
+            }
+        }
+        else -> {
+            println("Unknown command: ${args[0]}")
+            exitProcess(1)
+        }
+    }
+         
+}
 
-         val gitDir = File(".git")
-         gitDir.mkdir()
-         File(gitDir, "objects").mkdir()
-         File(gitDir, "refs").mkdir()
-         File(gitDir, "HEAD").writeText("ref: refs/heads/master\n")
 
-         println("Initialized git directory")
-    } else {
-        println("Unknown command: ${args[0]}")
-        exitProcess(1)
+fun ByteArray.zlibDecompress(): String {
+    val inflater = Inflater()
+    val outputStream = ByteArrayOutputStream()
+
+    return outputStream.use {
+        val buffer = ByteArray(1024)
+
+        inflater.setInput(this)
+
+        var count = -1
+        while (count != 0) {
+            count = inflater.inflate(buffer)
+            outputStream.write(buffer, 0, count)
+        }
+
+        inflater.end()
+        outputStream.toString("UTF-8")
     }
 }
