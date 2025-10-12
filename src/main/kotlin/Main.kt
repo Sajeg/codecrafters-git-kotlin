277c6a05de33236b9f4e1f859b5a7481d846149f
Sajeg <84874195+Sajeg@users.noreply.github.com>
2025-10-12 22:16:35 +0000

    added support for creating tree objects

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@ import java.io.File
 import java.security.MessageDigest
 import java.util.zip.Deflater
 import java.util.zip.Inflater
+import kotlin.io.path.Path
+import kotlin.io.path.isSymbolicLink
 import kotlin.system.exitProcess
 
 class TreeObjects(
@@ -10,6 +12,8 @@ class TreeObjects(
     val name: String,
     val hash: String
 )
+
+val hexChars = "0123456789abcdef"
 
 @OptIn(ExperimentalStdlibApi::class)
 fun main(args: Array<String>) {
@@ -48,67 +52,53 @@ fun main(args: Array<String>) {
             } else {
                 args[1]
             }
-            File(path).inputStream().use { fileInputStream ->
-                val fileContent = fileInputStream.readAllBytes()
-                val hexChars = "0123456789abcdef"
-                val blob = "blob ${fileContent.size}\u0000".toByteArray(Charsets.UTF_8)
-                val bytes = MessageDigest
-                    .getInstance("SHA-1")
-                    .digest(blob.plus(fileContent))
-                val hash = StringBuilder(bytes.size * 2)
-
-                bytes.forEach {
-                    val i = it.toInt()
-                    hash.append(hexChars[i shr 4 and 0x0f])
-                    hash.append(hexChars[i and 0x0f])
-                }
-                println(hash)
-                if (!writeFile) {
-                    return
-                }
-                
-                val compressedBlob = blob.plus(fileContent).zlibCompress()
-                File(".git/objects/${hash.subSequence(0, 2)}/").mkdirs()
-                File(".git/objects/${hash.subSequence(0, 2)}/${hash.subSequence(2, 40)}").apply {
-                    createNewFile()
-                    writeBytes(compressedBlob)
-                }
-            }
+            println(createBlob(writeFile, path))
         }
 
-	"ls-tree" -> {
-		var nameOnly = false
-        val path = if (args[1] == "--name-only") {
-            nameOnly = true
-            args[2]
-        } else {
-            args[1]
-        }
-
-        File(".git/objects/${path.subSequence(0, 2)}/${path.subSequence(2, 40)}").inputStream().use { fileInputStream ->
-            val fileContent = fileInputStream.readAllBytes().zlibDecompress()
-            val treeObjects = mutableListOf<TreeObjects>()
-            var startPosition = fileContent.indexOf(0) + 1
-            while (startPosition < fileContent.size) {
-                val zeroPosition = fileContent.copyOfRange(startPosition, fileContent.size).indexOf(0.toByte()) + startPosition
-                val splitString = fileContent.copyOfRange(startPosition, zeroPosition).decodeToString().trim().split(' ')
-                treeObjects.add(TreeObjects(splitString[0], splitString[1], fileContent.copyOfRange(zeroPosition, zeroPosition + 20).toHexString()))
-                startPosition = zeroPosition + 21
-            }
-//            val directories = treeObjects.filter { it.permission == "40000" }.sortedBy { it.name.trim().lowercase() }
-//            val files = treeObjects.filter { it.permission != "40000" }.sortedBy { it.name.trim().lowercase() }
-            
-            if (nameOnly) {
-                treeObjects.sortedBy { it.name.trim().lowercase() }.forEach {  
-                    println(it.name)
-                }
+        "ls-tree" -> {
+            var nameOnly = false
+            val path = if (args[1] == "--name-only") {
+                nameOnly = true
+                args[2]
             } else {
-                treeObjects.sortedBy { it.name.trim().lowercase() }.forEach {
-                    println("${if (it.permission == "40000") "040000" else it.permission} ${if (it.permission == "40000") "tree" else "blob"} ${it.hash}\t${it.name}")
-                }
+                args[1]
             }
+
+            File(".git/objects/${path.subSequence(0, 2)}/${path.subSequence(2, 40)}").inputStream()
+                .use { fileInputStream ->
+                    val fileContent = fileInputStream.readAllBytes().zlibDecompress()
+                    val treeObjects = mutableListOf<TreeObjects>()
+                    var startPosition = fileContent.indexOf(0) + 1
+                    while (startPosition < fileContent.size) {
+                        val zeroPosition =
+                            fileContent.copyOfRange(startPosition, fileContent.size).indexOf(0.toByte()) + startPosition
+                        val splitString =
+                            fileContent.copyOfRange(startPosition, zeroPosition).decodeToString().trim().split(' ')
+                        treeObjects.add(
+                            TreeObjects(
+                                splitString[0],
+                                splitString[1],
+                                fileContent.copyOfRange(zeroPosition, zeroPosition + 20).toHexString()
+                            )
+                        )
+                        startPosition = zeroPosition + 21
+                    }
+
+                    if (nameOnly) {
+                        treeObjects.sortedBy { it.name.trim().lowercase() }.forEach {
+                            println(it.name)
+                        }
+                    } else {
+                        treeObjects.sortedBy { it.name.trim().lowercase() }.forEach {
+                            println("${if (it.permission == "40000") "040000" else it.permission} ${if (it.permission == "40000") "tree" else "blob"} ${it.hash}\t${it.name}")
+                        }
+                    }
+                }
         }
-	}
+        
+        "write-tree" -> {
+            createTree(File(""))
+        }
 
         else -> {
             println("Unknown command: ${args[0]}")
@@ -116,6 +106,75 @@ fun main(args: Array<String>) {
         }
     }
 
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+fun createTree(path: File): String {
+    val files = path.listFiles()
+    val treeObjects = mutableListOf<TreeObjects>()
+    files?.forEach { file ->
+        if (file.isDirectory) {
+            treeObjects.add(TreeObjects("40000", file.name, createTree(file)))
+        } else if (file.canExecute()) {
+            treeObjects.add(TreeObjects("100755", file.name, createBlob(true, file.path)))
+        } else if (Path(file.path).isSymbolicLink()) {
+            treeObjects.add(TreeObjects("120000", file.name, createBlob(true, file.path)))
+        } else {
+            treeObjects.add(TreeObjects("100644", file.name, createBlob(true, file.path))) 
+        }
+    }
+    var fileContent = ""
+    treeObjects.forEach { tree ->
+        fileContent += "${tree.permission} ${tree.name}\u0000${tree.hash.toByteArray().toHexString()}"
+    }
+    
+    val tree = "tree ${fileContent.length}\u0000".toByteArray(Charsets.UTF_8)
+    val bytes = MessageDigest
+        .getInstance("SHA-1")
+        .digest(tree.plus(fileContent.toByteArray()))
+    val hash = StringBuilder(bytes.size * 2)
+
+    bytes.forEach {
+        val i = it.toInt()
+        hash.append(hexChars[i shr 4 and 0x0f])
+        hash.append(hexChars[i and 0x0f])
+    }
+
+    val compressedTree = tree.plus(fileContent.toByteArray()).zlibCompress()
+    File(".git/objects/${hash.subSequence(0, 2)}/").mkdirs()
+    File(".git/objects/${hash.subSequence(0, 2)}/${hash.subSequence(2, 40)}").apply {
+        createNewFile()
+        writeBytes(compressedTree)
+    }
+    return hash.toString()
+}
+
+fun createBlob(writeFile: Boolean, path: String): String {
+    File(path).inputStream().use { fileInputStream ->
+        val fileContent = fileInputStream.readAllBytes()
+        val blob = "blob ${fileContent.size}\u0000".toByteArray(Charsets.UTF_8)
+        val bytes = MessageDigest
+            .getInstance("SHA-1")
+            .digest(blob.plus(fileContent))
+        val hash = StringBuilder(bytes.size * 2)
+
+        bytes.forEach {
+            val i = it.toInt()
+            hash.append(hexChars[i shr 4 and 0x0f])
+            hash.append(hexChars[i and 0x0f])
+        }
+        if (!writeFile) {
+            return hash.toString()
+        }
+
+        val compressedBlob = blob.plus(fileContent).zlibCompress()
+        File(".git/objects/${hash.subSequence(0, 2)}/").mkdirs()
+        File(".git/objects/${hash.subSequence(0, 2)}/${hash.subSequence(2, 40)}").apply {
+            createNewFile()
+            writeBytes(compressedBlob)
+        }
+        return hash.toString()
+    }
 }
 
 fun ByteArray.zlibCompress(): ByteArray {
