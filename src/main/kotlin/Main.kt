788e1487ea4857a6850da2f33c2ccf6bd05338df
Sajeg <84874195+Sajeg@users.noreply.github.com>
2025-10-13 09:30:59 +0000

    Changed a lin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,7 +24,6 @@ const val authorEmail = "sajeg@example.com"
 
 @OptIn(ExperimentalStdlibApi::class)
 fun main(args: Array<String>) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!")
 
     if (args.isEmpty()) {
